9ae77878502def7b6be5512ce7cd46f5f9f70b68
karun7842 <70625725+karun7842@users.noreply.github.com>
2024-12-22 10:20:59 +0530

    Update DoctorSchedule.java

--- src/main/java/com/his/AppointmentSchedulingSystem/model/DoctorSchedule.java
+++ src/main/java/com/his/AppointmentSchedulingSystem/model/DoctorSchedule.java
@@ -1,64 +1,63 @@
-package com.his.AppointmentSchedulingSystem.model;
+package com.appointment.his.model;
 
 import java.util.List;
 import java.util.Objects;
 
-import com.his.AppointmentSchedulingSystem.controller.AppointmentController;
+import com.appointment.his.controller.AppointmentController;
 
 public class DoctorSchedule {
-private Doctor doctor;
-private String date;
-//private List<String> availableSlots = AppointmentController.generateTimeSlots(Integer.parseInt(doctor.getStartTime().substring(0,2)), Integer.parseInt(doctor.getEndTime().substring(0,2)));
-private List<String> availableSlots;
+	private Doctor doctor;
+	private String date;
+	private List<String> availableSlots;
 
-public Doctor getDoctor() {
-	return doctor;
-}
-public void setDoctor(Doctor doctor) {
-	this.doctor = doctor;
-}
-public String getDate() {
-	return date;
-}
-public void setDate(String date) {
-	this.date = date;
-}
-public List<String> getAvailableSlots() {
-	return availableSlots;
-}
-public void setAvailableSlots(List<String> availableSlots) {
-	this.availableSlots = availableSlots;
-}
-public DoctorSchedule(Doctor doctor, String date) {
-	super();
-	this.doctor = doctor;
-	this.date = date;
-	availableSlots = AppointmentController.generateTimeSlots(Integer.parseInt(doctor.getStartTime().substring(0, 2)),Integer.parseInt(doctor.getEndTime().substring(0, 2)));
+	public Doctor getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public List<String> getAvailableSlots() {
+		return availableSlots;
+	}
+	public void setAvailableSlots(List<String> availableSlots) {
+		this.availableSlots = availableSlots;
+	}
+	public DoctorSchedule(Doctor doctor, String date) {
+		super();
+		this.doctor = doctor;
+		this.date = date;
+		availableSlots = AppointmentController.generateTimeSlots(Integer.parseInt(doctor.getStartTime().substring(0, 2)),Integer.parseInt(doctor.getEndTime().substring(0, 2)));
 
-}
-@Override
-public int hashCode() {
-	return Objects.hash(date, doctor);
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	DoctorSchedule other = (DoctorSchedule) obj;
-	return Objects.equals(date, other.date) && Objects.equals(doctor, other.doctor);
-}
-public DoctorSchedule() {
-	super();
-	// TODO Auto-generated constructor stub
-}
-@Override
-public String toString() {
-	return "DoctorSchedule [doctor=" + doctor + ", date=" + date + ", availableSlots=" + availableSlots + "]";
-}
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, doctor);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DoctorSchedule other = (DoctorSchedule) obj;
+		return Objects.equals(date, other.date) && Objects.equals(doctor, other.doctor);
+	}
+	public DoctorSchedule() {
+		super();
+
+	}
+	@Override
+	public String toString() {
+		return "DoctorSchedule [doctor=" + doctor + ", date=" + date + ", availableSlots=" + availableSlots + "]";
+	}
 
 
 
