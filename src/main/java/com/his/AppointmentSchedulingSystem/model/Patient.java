b14193713e41dabb0454e34480c860498090e116
karun7842 <70625725+karun7842@users.noreply.github.com>
2024-12-22 10:21:22 +0530

    Update Patient.java

--- src/main/java/com/his/AppointmentSchedulingSystem/model/Patient.java
+++ src/main/java/com/his/AppointmentSchedulingSystem/model/Patient.java
@@ -1,94 +1,159 @@
-package com.his.AppointmentSchedulingSystem.model;
+
+package com.appointment.his.model;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-
-import com.his.AppointmentSchedulingSystem.controller.AppointmentController;
 
 public class Patient {
-	static private int tokenCounter;
-    private String name;
+	private static int tokenCounter;
+	private int mrdID;
+	private String firstName;
+	private String lastName;
 	private LocalDate dob;
-    private int age;
-    private String address;
-    private String email;
-    private String bloodGroup;
-    private long phoneNumber;
-    private int mrdID;
-    public Patient() {
-    	
-    }
- 
-   
-    public Patient(String name, LocalDate dob, int age, String address, String email, String bloodGroup, long phoneNumber) {
-        this.name = name;
-        this.dob = dob;
-        this.age = age;
-        this.address = address;
-        this.email = email;
-        this.bloodGroup = bloodGroup;
-        this.phoneNumber = phoneNumber;
-        this.mrdID=++tokenCounter;
-    }
- 
-    public String getName() {
-    	return name;
-    	}
-    public LocalDate getDob() {
-    	return dob;
-    	}
-    public int getAge() {
-    	return age;
-    	}
-    public String getAddress() {
-    	return address;
-    	}
-    public String getEmail() {
-    	return email;
-    	}
-    public String getBloodGroup() {
-    	return bloodGroup;
-    	}
-    public long getPhoneNumber() {
-    	return phoneNumber;
-    	}
-    public int getTokenNumber() {
-    	return mrdID;
-    }
-    public void setTokenNumber(int tokenNumber) {
-    	this.mrdID=tokenNumber;
-    }
-    public void setName(String name) {
-		this.name = name;
+	private int ageYears;
+	private int ageMonths;
+	private String sex;
+	private String maritalStatus;
+	private String bloodGroup;
+	private String email;
+	private long phoneNumber;
+	private String address;
+	private String pincode;
+	private String state;
+	private String emergencyContactName;
+	private long emergencyContactNumber;
+
+	public Patient() {
+		super();
+
+	}
+	public Patient( String firstName, String lastName,LocalDate dob, int ageYears,int ageMonths,String sex, String maritalStatus,
+			String bloodGroup, String email, long phoneNumber, String address, String pincode, String state,String emergencyContactName,
+			long emergencyContactNumber) {
+		tokenCounter = tokenCounter +1;
+		this.mrdID = tokenCounter;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.ageYears=ageYears;
+		this.ageMonths=ageMonths;
+		this.sex = sex;
+		this.maritalStatus = maritalStatus;
+		this.bloodGroup = bloodGroup;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.pincode = pincode;
+		this.state = state;
+		this.emergencyContactName=emergencyContactName;
+		this.emergencyContactNumber=emergencyContactNumber;
+	}
+	// Getters and setters
+	public int getMrdID() {
+		return mrdID;
+	}
+	public void setMrdID(int mrdID) {
+		this.mrdID = mrdID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
 	}
 
-
+	public int getAgeYears() {
+		return ageYears;
+	}
+	public void setAgeYears(int ageYears) {
+		this.ageYears = ageYears;
+	}
+	public int getAgeMonths() {
+		return ageMonths;
+	}
+	public void setAgeMonths(int ageMonths) {
+		this.ageMonths = ageMonths;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+	public String getEmail() {
+		return email;
+	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
 	public void setPhoneNumber(long phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-    
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
 
-    
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
 	public static int getTokenCounter() {
 		return tokenCounter;
 	}
-
-
 	public static void setTokenCounter(int tokenCounter) {
 		Patient.tokenCounter = tokenCounter;
 	}
-
-
-	@Override
-	public String toString() {
-		return new Integer(mrdID).toString();
+	public String getEmergencyContactName() {
+		return emergencyContactName;
+	}
+	public void setEmergencyContactName(String emergencyContactName) {
+		this.emergencyContactName = emergencyContactName;
+	}
+	public long getEmergencyContactNumber() {
+		return emergencyContactNumber;
+	}
+	public void setEmergencyContactNumber(long emergencyContactNumber) {
+		this.emergencyContactNumber = emergencyContactNumber;
 	}
 
 
-    
+
+
 }
