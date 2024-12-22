6fee2ba54995cea28b21871b626b4c219bdeeb76
karun7842 <70625725+karun7842@users.noreply.github.com>
2024-12-22 10:20:36 +0530

    Update Doctor.java

--- src/main/java/com/his/AppointmentSchedulingSystem/model/Doctor.java
+++ src/main/java/com/his/AppointmentSchedulingSystem/model/Doctor.java
@@ -1,29 +1,60 @@
-package com.his.AppointmentSchedulingSystem.model;
+package com.appointment.his.model;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
-
 public class Doctor {
-    private static int idCounter ;
-    private int id;
-    private String name;
+	private static int idCounter;  
+	private int id;
+	private String name;
+	private String lname;
+	private String gender;
+
 	private String contact;
 	private String email;
-	
-	
 	private String address;
+	private String adress2;
+	private String adress3;
+	private String pincode;
+	private String state;
 	private LocalDate dob;
-    private int age;
-    private String department;
-    private String specialization;
-    private String qualification;
-    private String startTime;
-    private String endTime;
-    private String availableFromDay;
-    private String availableToDay;
-    private double consultationFee;
- 
-    
+	private int age;
+	private String department;
+	private String specialization;
+	private String qualification;
+	private String startTime;
+	private String endTime;
+	private List<String> availableDays;
+
+
+	private double consultationFee;
+
+	public Doctor( String name,String lname,String gender, String contact, String email, String address,String adress2,String adress3,String pincode,String state,LocalDate dob, int age,
+			String department, String specialization, String qualification, String startTime, String endTime,
+			List<String> availableDays,double consultationFee) {
+		super();
+		this.id = idCounter++;
+		this.name = name;
+		this.lname=lname;
+		this.gender=gender;
+		this.contact = contact;
+		this.email = email;
+		this.address = address;
+		this.adress2=adress2;
+		this.adress3=adress3;
+		this.pincode=pincode;
+		this.state=state;
+		this.dob = dob;
+		this.age = age;
+		this.department = department;
+		this.specialization = specialization;
+		this.qualification = qualification;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.availableDays = availableDays;
+
+		this.consultationFee=consultationFee;
+	}
 	public Doctor() {
 		super();
 	}
@@ -31,63 +62,60 @@ public class Doctor {
 		return idCounter;
 	}
 	public int getId() {
-        return id;
-    }
- 
-    public String getName() {
-        return name;
-    }
- 
-    public String getContact() {
-        return contact;
-    }
-    public void setConsultationFee(double consultationFee) {
-    	this.consultationFee = consultationFee;
-    }
- 
-    public int getAge() {
-        return age;
-    }
- 
-    public String getSpecialization() {
-        return specialization;
-    }
- 
-    public String getStartTime() {
-        return startTime;
-    }
- 
-    public String getEndTime() {
-        return endTime;
-    }
-    public static void setIdCounter(int idCounter) {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+	public static void setIdCounter(int idCounter) {
 		Doctor.idCounter = idCounter;
 	}
- 
+
 	public void setId(int id) {
 		this.id = id;
 	}
- 
+
 	public void setName(String name) {
 		this.name = name;
 	}
- 
+
 	public void setContact(String contact) {
 		this.contact = contact;
 	}
- 
+
 	public void setAge(int age) {
 		this.age = age;
 	}
- 
+
 	public void setSpecialization(String specialization) {
 		this.specialization = specialization;
 	}
- 
+
 	public void setStartTime(String startTime) {
 		this.startTime = startTime;
 	}
- 
+
 	public void setEndTime(String endTime) {
 		this.endTime = endTime;
 	}
@@ -103,11 +131,9 @@ public class Doctor {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public LocalDate getDob() {
 		return dob;
 	}
-	
 	public void setDob(LocalDate dob) {
 		this.dob = dob;
 	}
@@ -123,39 +149,64 @@ public class Doctor {
 	public void setQualification(String qualification) {
 		this.qualification = qualification;
 	}
-	public String getAvailableFromDay() {
-		return availableFromDay;
+	public List<String> getAvailableDays() {
+		return availableDays;
 	}
-	public void setAvailableFromDay(String availableFromDay) {
-		this.availableFromDay = availableFromDay;
+	public void setAvailableDays(List<String> availableDays) {
+		this.availableDays = availableDays;
 	}
-	public String getAvailableToDay() {
-		return availableToDay;
-	}
-	public void setAvailableToDay(String availableToDay) {
-		this.availableToDay = availableToDay;
-	}
- 
-	public Object[] toArray() {
-        return new Object[]{name, contact, email, address, dob, age, department, specialization,
-                qualification, startTime, endTime, availableFromDay, availableToDay};
-    }
 	public double getConsultationFee() {
-		// TODO Auto-generated method stub
 		return consultationFee;
 	}
-	@Override
-	public String toString() {
-		return "Doctor [id=" + id + ", name=" + name + ", contact=" + contact + ", email=" + email
-				+ ", consultationFee=" + consultationFee + ", address=" + address + ", dob=" + dob + ", age=" + age
-				+ ", department=" + department + ", specialization=" + specialization + ", qualification="
-				+ qualification + ", startTime=" + startTime + ", endTime=" + endTime + ", availableFromDay="
-				+ availableFromDay + ", availableToDay=" + availableToDay + "]";
+	public void setConsultationFee(double consultationFee) {
+		this.consultationFee = consultationFee;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAdress2() {
+		return adress2;
+	}
+	public void setAdress2(String adress2) {
+		this.adress2 = adress2;
+	}
+	public String getAdress3() {
+		return adress3;
+	}
+	public void setAdress3(String adress3) {
+		this.adress3 = adress3;
+	}
+	public String getPincode() {
+		return pincode;
+	}
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public Object[] toArray() {
+		return new Object[]{name,lname,gender,contact, email, address,adress2,adress3,pincode,state, dob, age, department, specialization,
+				qualification, startTime, endTime,String.join(",", availableDays) ,consultationFee};
 	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(address, age, availableFromDay, availableToDay, consultationFee, contact, department, dob,
-				email, endTime, id, name, qualification, specialization, startTime);
+		return Objects.hash(address, adress2, adress3, age, availableDays, consultationFee, contact, department, dob,
+				email, endTime, gender, id, lname, name, pincode, qualification, specialization, startTime, state);
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -166,16 +217,17 @@ public class Doctor {
 		if (getClass() != obj.getClass())
 			return false;
 		Doctor other = (Doctor) obj;
-		return Objects.equals(address, other.address) && age == other.age
-				&& Objects.equals(availableFromDay, other.availableFromDay)
-				&& Objects.equals(availableToDay, other.availableToDay)
+		return Objects.equals(address, other.address) && Objects.equals(adress2, other.adress2)
+				&& Objects.equals(adress3, other.adress3) && age == other.age
+				&& Objects.equals(availableDays, other.availableDays)
 				&& Double.doubleToLongBits(consultationFee) == Double.doubleToLongBits(other.consultationFee)
 				&& Objects.equals(contact, other.contact) && Objects.equals(department, other.department)
 				&& Objects.equals(dob, other.dob) && Objects.equals(email, other.email)
-				&& Objects.equals(endTime, other.endTime) && id == other.id && Objects.equals(name, other.name)
-				&& Objects.equals(qualification, other.qualification)
-				&& Objects.equals(specialization, other.specialization) && Objects.equals(startTime, other.startTime);
+				&& Objects.equals(endTime, other.endTime) && Objects.equals(gender, other.gender) && id == other.id
+				&& Objects.equals(lname, other.lname) && Objects.equals(name, other.name)
+				&& Objects.equals(pincode, other.pincode) && Objects.equals(qualification, other.qualification)
+				&& Objects.equals(specialization, other.specialization) && Objects.equals(startTime, other.startTime)
+				&& Objects.equals(state, other.state);
 	}
-	
 	
 }
